5e4942aced98269674fbe0805015f7a40dc4ee3b
Никита Воронков <nikitonvg@list.ru>
2021-04-18 18:55:46 +0300

    addTest

--- hahaRU/Storage/Entity/User.cs
+++ hahaRU/Storage/Entity/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,27 @@ namespace hahaRU.Storage.Entity
 {
     public class User
     {
-        [Key]
-        public int Id { get; set; }
+        public User(int id=0, int name=0, int secondName=0, string avatarSrc="", string status = "", string favoriteJoke = "", string vkLink = "", string telegram = "", string instaLink = "")
+        {
+            Id = id;
+            Name = name;
+            SecondName = secondName;
+            AvatarSrc = avatarSrc;
+            Status = status;
+            FavoriteJoke = favoriteJoke;
+            VkLink = vkLink;
+            Telegram = telegram;
+            InstaLink = instaLink;
+        }
 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
         public int Name { get; set; }
+        [Required]
         public int SecondName { get; set; }
+
         public string AvatarSrc { get; set; }
 
         public string Status { get; set; }
