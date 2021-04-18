1f43b0106d2055cb44b1ee2cbce07e7c5b16de35
Никита Воронков <nikitonvg@list.ru>
2021-04-18 13:26:03 +0300

    add Users and Post entity

---
+++ hahaRU/Storage/Entity/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hahaRU.Storage.Entity
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Likes { get; set; }
+        public string Dislikes { get; set; }
+
+    }
+}
---
+++ hahaRU/Storage/Entity/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hahaRU.Storage.Entity
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+        public int SecondName { get; set; }
+        public string AvatarSrc { get; set; }
+
+        public string Status { get; set; }
+        public string FavoriteJoke { get; set; }
+        public string VkLink { get; set; }
+        public string Telegram { get; set; }
+        public string InstaLink { get; set; }
+    }
+}
