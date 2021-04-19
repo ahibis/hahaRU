dc701120c7daf2417548f0d9ce14b9eb943b11e1
Никита Воронков <nikitonvg@list.ru>
2021-04-19 16:17:57 +0300

    change

---
+++ hahaRU/Migrations/20210418190115_initMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hahaRU.Migrations
+{
+    public partial class initMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
