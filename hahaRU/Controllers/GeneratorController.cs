3627f8c685baf58c9bca6d6de91efd4f63a88460
Никита Воронков <nikitonvg@list.ru>
2021-04-19 17:43:08 +0300

    АХАХАХАХАХ ХЭХЭХЭ

---
+++ hahaRU/Controllers/GeneratorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hahaRU.Controllers
+{
+    public class GeneratorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Jokes()
+        {
+            return View();
+        }
+        public IActionResult Word()
+        {
+            AHAHAHAH a = new AHAHAHAH();
+            ViewBag.word = a.word;
+            return View();
+        }
+    }
+}
